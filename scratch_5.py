3cbff7bc3eee5d3f9b26019aafe6784561215ffc
Denis Jacquemin <denis.jacquemin@gmail.com>
2023-01-02 13:03:36 +0100

    typo

--- scratch_5.py
+++ scratch_5.py
@@ -1,7 +1,7 @@
 # Exercice 4
 # Write a program that calculates the Fibonacci sequence.
 
-# function who create the Fibonacci sequence
+# function which create the Fibonacci sequence
 def FibonacciSequence():
     t1, t2 = 0, 1
     while t2 <= 1000000:
